83d1ace4e080adaa7d2fe331a5e4d94149d7f2b6
Kumar Kshitij <53141181+kshitij98k@users.noreply.github.com>
2020-10-03 10:01:50 +0530

    Updates

---
+++ precache-manifest.a535312c9779d3c2513c8d060d94575d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8253ad1a533d0450bac0a146f90e0bdf",
+    "url": "/reviomusic5/index.html"
+  },
+  {
+    "revision": "62f839acd9f22f5023fc",
+    "url": "/reviomusic5/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "9171dbbeeaa980719ec5",
+    "url": "/reviomusic5/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62f839acd9f22f5023fc",
+    "url": "/reviomusic5/static/js/2.83ea9789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviomusic5/static/js/2.83ea9789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9171dbbeeaa980719ec5",
+    "url": "/reviomusic5/static/js/main.83b37fa5.chunk.js"
+  },
+  {
+    "revision": "cfd5bcd66f899a15d6cf",
+    "url": "/reviomusic5/static/js/runtime-main.ce7598f9.js"
+  }
+]);
